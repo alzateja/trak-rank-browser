208ff84bce79fb0a343830c43cee6b69820421bb
alzateja <alzateja@gmail.com>
2017-05-10 09:40:48 -0400

    Commit refactored code - Album list Rendering
    The code for rendering the album list screen has been refactored.  I
    also added functionality to the page navigation bar. I made updates to
    the handlebars script, the Javascript files, and the HTML as well.

--- assets/scripts/albums/ui.js
+++ assets/scripts/albums/ui.js
@@ -55,7 +55,7 @@ const getRatingsFailure = (error) => {
 
 // CALCULATE STATS
 const calculateStats = function () {
-  console.log(store.albums)
+  // console.log(store.albums)
   const totalAlbums = store.albums.length
   let completeStat = 0
   let progressStat = 0
@@ -115,50 +115,94 @@ const resetUserStatsModal = () => {
   $('#userStatsForm').modal('hide')
 }
 
-// UI Rendering
-const clearContent = () => {
-  console.log('Album clear sucess')
-  $('#content').empty()
-}
+// RENDERING OF ALBUMS PAGE
 
 const renderAlbums = (start) => {
+  // Clear Contents
   clearContent()
-  let end = start + 49
-  let pages = Math.ceil(store.albums.length / 50)
-  generatePages(pages)
-
+  //  Define Variables
+  let showPerPage = 50
+  let end = start + (showPerPage - 1)
+  let pages = Math.ceil(store.albums.length / showPerPage)
+  let currentPage = (start / showPerPage) + 1
+  let renderCounter = 0
   let temp = []
+// Console log current Page
+  console.log('current page:' + currentPage)
+
+// Generate page Navigation
+  generatePageNav(pages, currentPage)
+
+// Check to see that end does not exceed total albums count
   if (end > store.albums.length) {
     end = store.albums.length - 1
   }
 
+// Render Loop for Albums
+
   for (let i = start; i <= end; i++) {
-    if (i => start && i <= end) {
-      temp.push(store.albums[i])
+    // if (renderCounter % 3 === 0 || renderCounter === 0) {
+    //   $('.content').append('<div class="row">')
+    // }
+    temp.push(store.albums[i])
+    renderCounter++
+
+    if (renderCounter === 3 || i === end) {
+      $('.content').append(showAlbumsTemplate({albums: temp}))
+      temp = []
+      renderCounter = 0
     }
   }
-
-  let showAlbumsHtml = showAlbumsTemplate({albums: temp})
-  $('.content').append(showAlbumsHtml)
   $('.rate-album').on('click', modalEvents.onViewAlbumDetail)
 }
 
-const generatePages = function(needed) {
+// GENERATE PAGE NAV
+const generatePageNav = function (needed, current) {
+// Empty the page navigation bar
   $('#page-lister').empty()
+
+// Complete the page navigation
+  if (current > 1) {
+    $('#page-lister').append('<li id="prev-page"><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>')
+  }
   for (let i = 1; i <= needed; i++) {
-    $('#page-lister').append('<li class=""><a href="#" id ="page-' + i + '">' + i + '<span class="sr-only">(current)</span></a></li>')
+    $('#page-lister').append('<li id ="page-' + i + '"><a href="#" >' + i + '<span class="sr-only">(current)</span></a></li>')
     $('#page-' + i).on('click', selectPage)
   }
+  if (current !== needed) {
+    $('#page-lister').append('<li id="next-page"><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>')
+  }
+
+  // Add active clase and lister for the
+  $('#page-' + current).addClass('active')
+
+  // Define functions to trigger click event
+  const setPreviousPage = function () {
+    $('#page-' + (current - 1)).trigger('click')
+  }
+  const setNextPage = function () {
+    $('#page-' + (current + 1)).trigger('click')
+  }
+
+// Add click handlers to the previous page
+  $('#prev-page').on('click', setPreviousPage)
+  $('#next-page').on('click', setNextPage)
 }
 
+// SELECT THE PAGE
 const selectPage = function (event) {
   event.preventDefault()
-  const elementId = event.currentTarget.id.replace('page-', '')
-  const num = elementId.toString()
-  console.log(num)
-  let start = (num - 1) * 50
-  console.log(start)
+  let showPerPage = 50
+  const num = event.currentTarget.id.replace('page-', '')
+  let start = (num - 1) * showPerPage
+  console.log('Rendering Albums From' + start)
   renderAlbums(start)
+}
+
+// Clear Screen
+const clearContent = () => {
+  console.log('Clear Album Content Display Screen')
+  $('#content').empty()
 }
 
 module.exports = {
@@ -172,5 +216,4 @@ module.exports = {
   calculateStats,
   resetUserStatsModal,
   renderAlbums
-
 }
